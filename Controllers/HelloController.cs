d725d0ba33950afd8dea8170af1ea8283ae42f92
srikanthpragada <srikanthpragada@gmail.com>
2017-01-25 10:05:42 +0530

    25-JAN

---
+++ Controllers/HelloController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello    Hello/Index
+        public ActionResult Index(string id = "Unknown")
+        {
+            ViewBag.Message = "Hello, " + id;
+            return View();
+        }
+
+        public ActionResult Wish(string name)
+        {
+            ViewBag.Message = "Hello, " + name;
+            return View();
+        }
+    }
+}
