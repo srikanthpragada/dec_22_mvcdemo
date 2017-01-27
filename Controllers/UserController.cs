439e422f01d56f7e724f0ea3bc6796937767e23f
srikanthpragada <srikanthpragada@gmail.com>
2017-01-27 10:01:30 +0530

    27-JAN

---
+++ Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using mvcdemo.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace mvcdemo.Controllers
+{
+    public class UserController : Controller
+    {
+        [HttpGet]
+        public ActionResult Login()
+        {
+            User u = new User { Email = "admin@abc.com" };
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Login(User user)
+        {
+            if (user.Email == "admin@abc.com" && user.Password == "123")
+                return RedirectToAction("Home");
+            else
+            {
+                ViewBag.Message = "Invalid Login. Try Again!";
+                return View(user);
+            }
+        }
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace mvcdemo.Models
+{
+    public class User
+    {
+        [EmailAddress (ErrorMessage ="Invalid Email Address")]
+        [Required (ErrorMessage ="Missing Email Address")]
+        public String Email { get; set; }
+
+        [Required]
+        [StringLength(10,MinimumLength = 4, ErrorMessage = "Password Must Be 4 to 10 Chars")]
+        public String Password { get; set; }
+    }
+}
