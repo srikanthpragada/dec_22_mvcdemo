dd77968bfd195b4985000255876ec583fa4c59d4
srikanthpragada <srikanthpragada@gmail.com>
2017-02-06 10:01:02 +0530

    6-FEB

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -10,21 +10,54 @@ namespace mvcdemo.Controllers
 {
     public class CoursesController : ApiController
     {
-        List<Course> courses = new List<Course>
+        [HttpGet]
+        public IEnumerable<LinqCourse> GetCourses()
         {
-            new Course { Name = "Microsoft.Net", Duration = 55, Fee = 5500 },
-            new Course { Name = "Java SE 8.0", Duration = 40, Fee = 3500 },
-            new Course { Name = "Java EE 7.0", Duration = 50, Fee = 4000 }
-        };
-
-        public List<Course> GetCourses()
-        {
-            return courses;
+            MyDataContext dc = new MyDataContext();
+            return dc.Courses;
         }
 
-        public Course GetCourse(int id)
+        [HttpGet]
+        public IHttpActionResult GetCourse(String id )
         {
-            return courses[id-1];
+            MyDataContext dc = new MyDataContext();
+            var course = dc.Courses.Where(c => c.Code == id).SingleOrDefault();
+            if (course != null)
+                return Ok(course);
+            else
+                return NotFound();
+        }
+
+        [HttpPost]
+        public void AddCourse(LinqCourse course)
+        {
+            try {
+                MyDataContext dc = new MyDataContext();
+                dc.Courses.InsertOnSubmit(course);
+                dc.SubmitChanges();
+            }
+            catch(Exception ex)
+            {
+                HttpResponseMessage msg = new HttpResponseMessage();
+                msg.StatusCode = HttpStatusCode.InternalServerError;
+                msg.ReasonPhrase = "Error while adding course -> " + ex.Message;
+                throw new HttpResponseException(msg);
+            }
+        }
+
+        [HttpDelete]
+        public IHttpActionResult DeleteCourse(String id)
+        {
+            MyDataContext dc = new MyDataContext();
+            var course = dc.Courses.Where(c => c.Code == id).SingleOrDefault();
+            if (course != null)
+            {
+                dc.Courses.DeleteOnSubmit(course);
+                dc.SubmitChanges();
+                return Ok();
+            }
+            else
+                return NotFound();
         }
     }
 }
